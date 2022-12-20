c90259e2f0dc6a7dae084295e75bd06f105656e3
Tor Henning Egenes <tor.henning.egenes@gmail.com>
2022-12-20 22:57:00 +0100

    working with decorators

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,37 @@
 // Decorators are for classes
 // Decorators are functions you apply to f.eks classes
 // Decorators run when classes are defined, not when instanciated
-function Logger(constructor: Function) {
-  console.log("Logging...");
-  console.log(constructor);
+// Decorators executes bottom up.
+function Logger(logString: string) {
+  console.log("LOGGER FACTORY");
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
-@Logger
+function withTemplate(template: string, hookId: string) {
+  console.log("TEMPLATE FACTORY");
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    Originalconstructor: T
+  ) {
+    return class extends Originalconstructor {
+      constructor(..._: any[]) {
+        super();
+        console.log("Rendering template");
+        const hookEl = document.getElementById(hookId);
+
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector("h1")!.textContent = this.name;
+        }
+      }
+    };
+  };
+}
+
+//@Logger("Logging - Person")
+@Logger("Logging")
+@withTemplate("<h1>My person object</h1>", "app")
 class Human {
   name = "Max";
   constructor() {
@@ -15,6 +41,58 @@ class Human {
   }
 }
 
-// const hum = new Human();
+const hum = new Human();
 
 // console.log(hum);
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("Parameter decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price");
+    }
+  }
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
+
+const p1 = new Product("Book", 19);
+const p2 = new Product("Book2", 29);
